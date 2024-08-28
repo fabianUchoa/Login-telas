6ca8ae8ea6652258eb24899e563b1ae2775f9a80
fabianUchoa <fabian@forlogic.net>
2024-08-28 13:31:30 -0300

    update

---
+++ Telas/script.js
@@ -0,0 +1,32 @@
+class user{
+    constructor(login, password){
+    }
+}
+
+let usuario = new user()
+
+function valida(){
+ 
+
+
+}
+
+function cadastrar(){
+
+    let mail = document.getElementById('user')
+    let password = document.getElementById('password')
+
+
+}
+
+function verificaPW(){
+    
+    let password = document.querySelector('#password')
+    let password2 = document.querySelector('#password2')
+
+    if(password === password2){
+        cadastrar(password)
+    }else{
+        /*DEVOLVE TEXTO DIZENDO QUE AS SENHAS NAO SAO IGUAIS*/
+    }
+}
